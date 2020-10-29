77231f34e533b65bf8d359aae59b35e4ee43b3b7
Tharun sekar <tharunsd23@gmail.com>
2020-10-29 17:11:30 +0530

    first commit

---
+++ build/precache-manifest.52e6a2d56db5a182ce0fb3161ffd99bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36e6967709b551956f490d5ac2f7c86f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cf8eebe412597e18da5b",
+    "url": "/static/css/main.7f80841f.chunk.css"
+  },
+  {
+    "revision": "a782f561bb9ea395913f",
+    "url": "/static/js/2.3fd55cbe.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/static/js/2.3fd55cbe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf8eebe412597e18da5b",
+    "url": "/static/js/main.301af4c6.chunk.js"
+  },
+  {
+    "revision": "2a5e9718c304aba39f84",
+    "url": "/static/js/runtime-main.0f5541c3.js"
+  }
+]);
